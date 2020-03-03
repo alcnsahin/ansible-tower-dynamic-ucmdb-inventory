af043933f3dcafae69a62559c44d8fa542ba4f7e
alcnsahin <alcnsahin@gmail.com>
2020-03-03 17:24:43 +0300

    library has changed

--- main.py
+++ main.py
@@ -15,8 +15,19 @@ auth_params = {
 }
 
 # authentication
-auth_call = requests.post(UCMDB_ENDPOINT + "/authenticate", data=auth_params, headers=headers)
+auth_call = requests.post(UCMDB_ENDPOINT + "/authenticate", json=auth_params, headers=headers, verify=False)
 print(auth_call.status_code)
 print("---------------------------------")
-print(auth_call.json())
+print(auth_call.json()["token"])
+print("---------------------------------")
 # execute tql query
+execute_tql_headers = {
+    "Authorization": "Bearer " + auth_call.json()["token"],
+    "Content-Type": "application/json"
+}
+execute_tql_params = {
+    "tqlName": "Redhat_Computers"
+}
+execute_tql = requests.post(UCMDB_ENDPOINT + "/topology", json=execute_tql_params, headers=execute_tql_headers,
+                            verify=False)
+print(execute_tql.json())
