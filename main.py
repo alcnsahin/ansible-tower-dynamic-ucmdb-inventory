29793959ccac51b77eadeb2bfd6f8e6133a87efb
alcnsahin <alcnsahin@gmail.com>
2020-03-03 11:45:49 +0300

    auth params converted to variables

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from urllib.request import Request
 import json
 
 UCMDB_ENDPOINT = "https://cmdb-hostname:port/api"
+UCMDB_USERNAME = 'username'
+UCMDB_PASSWORD = 'password'
+UCMDB_CLIENT_CONTEXT = 1
 headers = {
     "Content-Type": "application/json"
 }
 auth_params = {
-    'username': '',
-    'password': '',
-    'clientContext': 1
+    'username': UCMDB_USERNAME,
+    'password': UCMDB_PASSWORD,
+    'clientContext': UCMDB_CLIENT_CONTEXT
 }
 
 auth_call = Request(UCMDB_ENDPOINT, json.dumps(auth_params).encode('ascii'), headers, method='POST')
